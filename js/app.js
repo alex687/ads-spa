d7ca6d396da8fd9d3ec14322eaf644262d154961
alex687 <alex.aramov@gmail.com>
2014-12-24 02:05:59 +0200

    Adding some of the routing and starting creating the home page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+var adsApp = angular
+    .module('adsApp', ['ngResource', 'ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'templates/home.html'
+            })
+            .when('/login', {
+                templateUrl: 'templates/login.html'
+            })
+            .when('/register', {
+                templateUrl: 'templates/register.html'
+            })
+            .when('/user/home', {
+                templateUrl: 'templates/edit-artist.html'
+            })
+            .otherwise({redirectTo: '/'});
+    });
