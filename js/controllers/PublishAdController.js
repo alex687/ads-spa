1357ecedf4cf11c91e0089ce3bfe9f363c8c60a9
alex687 <alex_aramov@abv.bg>
2015-01-06 18:51:20 +0200

    Publish Contrroller

--- js/controllers/PublishAdController.js
+++ js/controllers/PublishAdController.js
@@ -1,5 +1,17 @@
 'use strict';
 
 adsApp.controller('PublishAdController', function HomeController($scope, adsData, authorization) {
+    $scope.publishAdAlert = false;
+    $scope.publish = function (ad) {
+        userData.login().success(function (data) {
+            $scope.publishAdAlert = false;
+            authorization.saveCredentials(data);
+            $state.go('home');
+        }).error(function (data) {
+            $scope.publishAdAlert = true;
+            $scope.alertMessage = data.error_description
+        });
+    };
 
+    $scope.$emit('changePageName', 'Publish new Ad');
 });
