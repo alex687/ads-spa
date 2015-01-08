4ba9f4736a88e4580424b23f68ac557a18044e2c
alex687 <alex_aramov@abv.bg>
2015-01-09 00:49:36 +0200

    User profile edit controller adding edit profile and change passwords functions

--- js/controllers/UserProfileEdit.js
+++ js/controllers/UserProfileEdit.js
@@ -1,5 +1,26 @@
 'use strict';
 
-adsApp.controller('UserProfileEdit', function UserProfileEdit($scope, authorization, pageSize) {
+adsApp.controller('UserProfileEdit', function UserProfileEdit($scope, userData) {
+    $scope.emailValidationPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    $scope.passwordPattern = /^[\s\S]{2,100}$/;
 
+    $scope.editProfile = function (user) {
+        userData.editProfile(user.name, user.email, user.phone, user.townId).$promise.then(function (data) {
+                //TODO Show success
+            },
+            function () {
+                //TODO Show error
+            });
+    };
+
+    $scope.changePassword = function (user) {
+        userData.changePassword(user.oldPassword, user.newPassword, user.confirmPassword).$promise.then(function (data) {
+                //TODO Show success
+            },
+            function () {
+                //TODO Show error
+            });
+    };
+
+    //TODO load towns
 });
